313c29331eeac045b95e39c8fa31a9f3781e9b6d
Rene Santiago <rsj510@gmail.com>
2023-01-07 18:39:43 -0500

    frontend complete

---
+++ src/loaders.js
@@ -0,0 +1,14 @@
+// Base URL of out deployed API
+const URL = "https://express-react-sige.onrender.com"
+
+export const projectsLoader = async () => {
+    const response = await fetch(URL + "/projects")
+    const projects = await response.json()
+    return projects
+}
+
+export const aboutLoader = async () => {
+    const response = await fetch(URL + "/about")
+    const about = await response.json()
+    return about
+}
